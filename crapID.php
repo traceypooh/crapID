7dd633d7717bad337f6e8c0af88d45e9b705c28a
Tracey Jaquith <tracey@vm-home1.archive.org>
2015-01-24 20:13:36 +0000

    basic visualization page

---
+++ crapID.php
@@ -0,0 +1,55 @@
+<? require_once('/petabox/setup.inc');
+
+Nav::head("The Craptioning Experiment");
+
+chdir("/var/tmp/tv/ADS") || fatal("bad box");
+
+
+$ao = Paths::serverSF(0);
+
+
+$matfi = glob("*.matches");
+echo "<h3>Picking 100 ADs at random from ".count($matfi)." matched ADs</h3>";
+
+shuffle($matfi);
+$matfi = array_slice($matfi,0,100);
+
+echo '<table class="tablesorter  table table-striped table-condensed table-hovertable"><tbody>';
+echo '
+<tr>
+  <th>AD (10 seconds in, length 10 seconds)</th>
+  <th>craptioned text the AD</th>
+  <th>best 10 matches (score, clip)</th>
+</tr>
+';
+
+
+foreach ($matfi as $fi){
+  //msg($fi);
+  if (!preg_match('/^([^,]+),([^,]+),([^\-]+)\-(\d+)\.txt\.hash\.matches$/', $fi, $mat))
+    continue;
+  list(,$id,$start,$end,$seek) = $mat;
+  $start += $seek;
+  $end   += $end;
+
+  echo "<tr><td><a href=\"//$ao/details/$id#start/$start/end/$end\">$id#start/$start/end/$end</a></td><td>";
+  
+  $txt = preg_replace('/\.hash\.matches$/','',$fi);
+  //echo file_get_contents($txt);
+  echo `cat $txt |cut -f3- |perl -pe 's/\(\d+\)\$//'  |egrep -v '^<s|sil|/s>\$'`;
+
+  $best = '';
+  foreach (Util::cmd("cat $fi |head -10","ARRAY","CONTINUE") as $line){
+    if (!preg_match('=^(\S+)\s+\./[^/]+/([^/]+)\-(\d+)\.txt\.hash$=', $line, $mat))
+      continue;
+    list(,$score,$id2,$start2)=$mat;
+    $start2=ltrim($start2,"0");
+    $best .= "$score <a href=\"//$ao/details/$id2#start/$start2/end/".($start2+30)."\">$id2</a>\n";
+  }
+  
+  echo "</td><td><pre>$best</pre></td></tr>";
+}
+
+echo "</tbody></table>";
+
+  
